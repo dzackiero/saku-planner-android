14c6e4e107f91f0dede3ea146edb01f9896af48e
Fitri Sagita <120694803+fitristachan@users.noreply.github.com>
2025-07-03 21:03:33 +0700

    add image on ScanResponse.kt

--- app/src/main/java/com/pnj/saku_planner/kakeibo/data/remote/dto/ScanResponse.kt
+++ app/src/main/java/com/pnj/saku_planner/kakeibo/data/remote/dto/ScanResponse.kt
@@ -2,6 +2,7 @@ package com.pnj.saku_planner.kakeibo.data.remote.dto
 
 data class ScanResponse(
     val status: String?,
+    val image: String?,
     val error_msg: String?,
     val tax: Long?,
     val items: List<Item>?
